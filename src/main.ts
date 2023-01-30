0993ca6d254355e5b38f0ca9fb8c72f74c69352f
Script Raccoon <scriptraccoon@gmail.com>
2023-01-30 09:07:20 +0100

    comments, refactoring

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./app.css";
 import { vocable } from "./types";
 import { vocables } from "./vocables";
 
+// setup of HTML elements
 const word_de = document.getElementById("word_de")!;
 const word_en = document.getElementById(
 	"word_en"
@@ -12,20 +13,23 @@ const next_btn = document.getElementById(
 	"next_btn"
 ) as HTMLButtonElement;
 
-let random_vocable: vocable;
+// current vocable which is displayed in HTML
+let current_vocable: vocable;
 
+// initialize when HTML is ready
 document.addEventListener("DOMContentLoaded", init);
 
+// initialize vocable and event listeners
 function init() {
-	display_random_vocable();
-	word_en.focus();
+	display_next_vocable();
 	form.addEventListener("submit", (e) => {
 		e.preventDefault();
 		show_result();
 	});
-	next_btn.addEventListener("click", display_random_vocable);
+	next_btn.addEventListener("click", display_next_vocable);
 }
 
+// get a random vocable from the vocables list
 function get_random_vocable() {
 	const number_of_vocables = vocables.length;
 	const random_index = Math.floor(
@@ -34,17 +38,19 @@ function get_random_vocable() {
 	return vocables[random_index];
 }
 
-function display_random_vocable() {
+// display the next vocable in HTML
+function display_next_vocable() {
 	result.innerHTML = "";
 	next_btn.style.display = "none";
-	random_vocable = get_random_vocable();
-	word_de.innerText = random_vocable["de"];
+	current_vocable = get_random_vocable();
+	word_de.innerText = current_vocable["de"];
 	word_en.value = "";
 	word_en.focus();
 }
 
+// show the result (if guess was correct) in HTML
 function show_result() {
-	const correct_word = random_vocable["en"];
+	const correct_word = current_vocable["en"];
 	const is_correct = word_en.value.trim() == correct_word;
 	const result_text = is_correct
 		? "Das ist richtig!"
