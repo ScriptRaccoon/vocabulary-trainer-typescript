2610c37629ce0e70c5feb45df4ba48089d0f819a
Script Raccoon <scriptraccoon@gmail.com>
2023-01-30 09:03:42 +0100

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -17,23 +17,27 @@ let random_vocable: vocable;
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-	generate_random_vocable();
+	display_random_vocable();
 	word_en.focus();
 	form.addEventListener("submit", (e) => {
 		e.preventDefault();
 		show_result();
 	});
-	next_btn.addEventListener("click", generate_random_vocable);
+	next_btn.addEventListener("click", display_random_vocable);
 }
 
-function generate_random_vocable() {
-	result.innerHTML = "";
-	next_btn.style.display = "none";
+function get_random_vocable() {
 	const number_of_vocables = vocables.length;
 	const random_index = Math.floor(
 		Math.random() * number_of_vocables
 	);
-	random_vocable = vocables[random_index];
+	return vocables[random_index];
+}
+
+function display_random_vocable() {
+	result.innerHTML = "";
+	next_btn.style.display = "none";
+	random_vocable = get_random_vocable();
 	word_de.innerText = random_vocable["de"];
 	word_en.value = "";
 	word_en.focus();
@@ -42,10 +46,11 @@ function generate_random_vocable() {
 function show_result() {
 	const correct_word = random_vocable["en"];
 	const is_correct = word_en.value.trim() == correct_word;
-	const text = is_correct
+	const result_text = is_correct
 		? "Das ist richtig!"
-		: `Das ist nicht richtig. Die richtige Übersetzung lautet: <b>${correct_word}</b>`;
-	result.innerHTML = text;
+		: "Das ist nicht richtig. Die richtige Übersetzung lautet: " +
+		  `<b>${correct_word}</b>`;
+	result.innerHTML = result_text;
 	next_btn.style.display = "inline";
 	next_btn.focus();
 }
